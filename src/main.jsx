db4f6f0ebc911269bd3ed28607ff175a7c19a4df
Brabyns Yabwetsa <burabuyabz@gmail.com>
2026-02-23 04:50:41 +0300

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,36 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter, Route, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in'
+import path from 'node:path'
+import Home from './home'
+import Dashboard from './dashboard'
+
+const router = createBrowserRouter([
+  {
+    
+    element: <App />,
+    children:[
+      {
+        path:'/', 
+        element: <Home />
+      },
+      {
+        path:'/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  },
+  {
+    path:'/auth/sign-in',
+    element: <SignInPage />
+  }
+])
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
